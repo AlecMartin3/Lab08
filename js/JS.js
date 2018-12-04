f8fc02b49c3661a9662e36474d60a24a30c657dd
Alec Martin <acmartin1@live.ca>
2018-12-04 09:38:41 -0800

    html works

--- js/JS.js
+++ js/JS.js
@@ -2,14 +2,14 @@ $("#leagueStandings").toggle();
 $(document).ready(function(){
     $("#scores").click(function(){
         $("#leagueStandings").toggle();
-        $(".container").toggle();
+        $("#teams").toggle();
     });
 });
 
 $(document).ready(function(){
     $("#standings").click(function(){
         $("#leagueStandings").toggle();
-        $(".container").toggle();
+        $("#teams").toggle();
     });
     $('#standings').click(function(e) {
 
@@ -23,7 +23,7 @@ $(document).ready(function(){
             data: { format: "html-list"},
             success: function(data) {
                 console.log("SUCCESS HTML:", data);
-                $("#content").html(data);
+                $("#leagueStandings").html(data);
     
             },
             error: function(jqXHR, textStatus, errorThrown) {
@@ -31,6 +31,33 @@ $(document).ready(function(){
                 console.log("ERROR:", jqXHR, textStatus, errorThrown);
             }
     
+        });
+    });
+    $('#scores').click(function(e) {
+
+        // don't allow the anchor to visit the link
+        e.preventDefault();
+
+        $.ajax({
+            url: "/ajax-GET-list",
+            dataType: "json",
+            type: "GET",
+            data: { format: "json-list"},
+            success: function(data) {
+                console.log("SUCCESS JSON:", data);
+                var div = $("#leagueStandings");
+                let htmlStr = "<ol>";
+                for(let i = 0; i < data.length; i++) {
+                    htmlStr += "<li>" + data[i] + "</li>";
+                }
+                htmlStr += "</ol>";
+                div.html(htmlStr);
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $("#p1").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
         });
     });
 });
--- js/express.js
+++ js/express.js
@@ -1,6 +1,9 @@
+const lists = require('./core/data');
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
 
 // so '/stuff' will map to the directory '/files'
 // try: http://localhost:8000/stuff/week08.html
@@ -11,7 +14,55 @@ app.use('/js', express.static('../js'));
 app.get('/', function(req, res) {
     res.sendfile(path.join(__dirname + '\\..\\html\\index.html'))
 })
+app.get('/ajax-GET', function (req, res) {
 
+    // set the type of response:
+    res.setHeader('Content-Type', 'application/json');
+    let d = new Date();
+
+    res.send({ msg: d });
+
+})
+app.get('/ajax-GET-list', function (req, res) {
+
+    //res.setHeader('Content-Type', 'application/json');
+    //console.log(req.query['format']);
+    let formatOfResponse = req.query['format'];
+    let dataList = null;
+
+    if(formatOfResponse == 'html-list') {
+
+        res.setHeader('Content-Type', 'text/html');
+        dataList = lists.getHTML();
+        res.send(dataList);
+
+    } else if(formatOfResponse == 'json-list') {
+
+        res.setHeader('Content-Type', 'application/json');
+        dataList = lists.getJSON();
+        res.send(dataList);
+
+    } else {
+        res.send({msg: 'Wrong format!'});
+    }
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Notice that this is a 'POST'
+app.post('/post-form', function (req, res) {
+      res.setHeader('Content-Type', 'application/json');
+
+      console.log("Stuff sent to server", req.body);
+
+      res.send(["You sent me:", req.body]);
+
+});
+app.use(function (req, res, next) {
+    res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+  })
 let port = 8000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + '!');
