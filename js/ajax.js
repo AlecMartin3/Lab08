301589c3d4323a73498d4d96c297ad8abb067a62
Alec Martin <acmartin1@live.ca>
2018-12-03 19:22:43 -0800

    express

--- js/ajax.js
+++ js/ajax.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 // so '/stuff' will map to the directory '/files'
 // try: http://localhost:8000/stuff/week08.html
-app.use('../css', express.static('css'))
-app.use('/img', express.static('img'))
-app.use('/js', express.static('js'));
+app.use('/css', express.static('../css'))
+app.use('/img', express.static('../img'))
+app.use('/js', express.static('../js'));
 
 app.get('/', function(req, res) {
     res.sendfile(path.join(__dirname + '\\..\\html\\index.html'))
